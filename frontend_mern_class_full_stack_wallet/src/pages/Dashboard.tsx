0edeb2b8db5b9ec42ebbb5598ffa4abd51d48b7c
Sujan STC <sujandstc@gmail.com>
2024-03-25 16:24:16 +0545

    Changes

--- frontend_mern_class_full_stack_wallet/src/pages/Dashboard.tsx
+++ frontend_mern_class_full_stack_wallet/src/pages/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { Button, Popconfirm, Popover } from "antd";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -35,20 +36,37 @@ const Dashboard = () => {
 
   return (
     <>
-      <div className="p-10">
-        Welcome {userDetails.name}
-        <br />
-        <br />
-        <br />
-        <br />
-        <a
-          onClick={() => {
-            localStorage.removeItem("accessToken");
-            navigate("/login");
-          }}
-        >
-          Logout
-        </a>
+      <div className="bg-gradient-to-r from-indigo-500 to-red-400 p-5 text-white font-bold">
+        <div className="flex justify-between items-center">
+          <div>Welcome {userDetails.name},</div>
+
+          <Popover
+            content={
+              <>
+                <hr />
+                <Popconfirm
+                  title="Logout"
+                  description="Are you sure to logout?"
+                  onConfirm={() => {
+                    localStorage.removeItem("accessToken");
+                    navigate("/login");
+                  }}
+                  showCancel={false}
+                  okText="Logout"
+                  okButtonProps={{ danger: true }}
+                >
+                  <div className="p-1 cursor-pointer">Logout</div>
+                </Popconfirm>
+              </>
+            }
+            title={userDetails.name}
+            trigger="click"
+          >
+            <div className=" bg-gray-700 p-3 rounded-full h-[35px] w-[35px] flex items-center text-white cursor-pointer">
+              {userDetails && userDetails.name ? userDetails.name[0] : ""}
+            </div>
+          </Popover>
+        </div>
       </div>
     </>
   );
--- frontend_mern_class_full_stack_wallet/src/pages/Login.tsx
+++ frontend_mern_class_full_stack_wallet/src/pages/Login.tsx
@@ -28,49 +28,56 @@ const Login = () => {
 
   return (
     <>
-      <div className=" p-10">
-        <Form
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          style={{ maxWidth: 600 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          autoComplete="off"
-        >
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[{ required: true, message: "Please input your email!" }]}
+      <div className="bg-gradient-to-r from-indigo-500 to-red-400  h-[100vh]  flex items-center justify-center">
+        <div className="p-10 bg-white/50 rounded-xl  w-[50vw]">
+          <h1 className="font-bold text-[25px]">Login to eWallet</h1>{" "}
+          <hr className="mt-2" />
+          <br />
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            autoComplete="off"
           >
-            <Input />
-          </Form.Item>
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[{ required: true, message: "Please input your email!" }]}
+            >
+              <Input />
+            </Form.Item>
 
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
-          >
-            <Input.Password />
-          </Form.Item>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
 
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="default" htmlType="submit">
-              Login
-            </Button>
-          </Form.Item>
-        </Form>
-        <br />
-        <br />
-        <Link className=" text-blue-800 font-bold" to={"/forgot-pw"}>
-          Forgot password?
-        </Link>
-        <br />
-        <br />
-        Don't have an account? <br /> <br />
-        <Link to={"/register"}>
-          <b> Create new account!</b>
-        </Link>
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button type="default" htmlType="submit" className="font-bold">
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+          <br />
+          <br />
+          <Link className=" text-blue-800 font-bold" to={"/forgot-pw"}>
+            Forgot password?
+          </Link>
+          <br />
+          <br />
+          Don't have an account? <br /> <br />
+          <Link to={"/register"}>
+            <b> Create new account!</b>
+          </Link>
+        </div>
       </div>
     </>
   );
